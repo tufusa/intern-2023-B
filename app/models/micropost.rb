b0978a8a897444dc3aea5a2c1c39d8e9e0c389ad
Tufusa <louptung@gmail.com>
2023-08-30 14:39:45 +0900

    add: `Micropost#content_url_splitted`を追加 contentをURLで分割した配列を返す

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,6 @@
 class Micropost < ApplicationRecord
+  include MicropostsHelper
+
   belongs_to :user
   has_one_attached :image do |attachable|
     attachable.variant :display, resize_to_limit: [500, 500]
@@ -7,7 +9,13 @@ class Micropost < ApplicationRecord
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
   validates :image,   content_type: { in: %w[image/jpeg image/gif image/png],
-                                      message: "must be a valid image format" },
-                      size:         { less_than: 5.megabytes,
-                                      message:   "should be less than 5MB" }
+                                      message: 'must be a valid image format' },
+                      size: { less_than: 5.megabytes,
+                              message: 'should be less than 5MB' }
+
+  def content_url_splitted
+    content.split(URL_EXP).map do |text|
+      { value: text, is_url: url?(text) }
+    end
+  end
 end
