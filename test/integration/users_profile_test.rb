2f1d09020b36ef928446175361653e6ee478a385
Tufusa <louptung@gmail.com>
2023-08-30 15:03:37 +0900

    test: `test_profile_display`を修正

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class UsersProfileTest < ActionDispatch::IntegrationTest
   include ApplicationHelper
@@ -7,7 +7,7 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
-  test "profile display" do
+  test 'profile display' do
     get user_path(@user)
     assert_template 'users/show'
     assert_select 'title', full_title(@user.name)
@@ -16,7 +16,7 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     assert_match @user.microposts.count.to_s, response.body
     assert_select 'div.pagination'
     @user.microposts.paginate(page: 1).each do |micropost|
-      assert_match micropost.content, response.body
+      assert_match micropost.content_html, response.body
     end
   end
 end
