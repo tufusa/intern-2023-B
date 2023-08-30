0b35d68b9c0f585040d942ffc548e4f3338854e6
hosogi <s215041@matsu.shimane-u.ac.jp>
2023-08-30 13:34:53 +0900

    表示画像のサイズ変更

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,7 +1,7 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   has_one_attached :image do |attachable|
-    attachable.variant :display, resize_to_limit: [500, 500]
+    attachable.variant :display, resize_to_limit: [300, 300]
   end
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
