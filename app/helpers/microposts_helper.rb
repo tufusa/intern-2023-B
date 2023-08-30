3b6869a239d128541f840918cab436df892b2e8f
Tufusa <louptung@gmail.com>
2023-08-30 14:37:12 +0900

    add: 文字列がURLか判定するメソッド`MicropostsHelper#url?`を追加

--- app/helpers/microposts_helper.rb
+++ app/helpers/microposts_helper.rb
@@ -1,2 +1,8 @@
 module MicropostsHelper
+  URL_EXP = %r{(https?://[a-z0-9-]+(?:\.[a-z]+)+(?:/[a-zA-Z0-9-]*)*[a-zA-Z0-9\-_?=&#]*)}.freeze
+
+  # stringがURLであればtrueを返す
+  def url?(string)
+    URL_EXP.match? string
+  end
 end
