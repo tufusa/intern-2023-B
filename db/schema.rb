caea61d7af7a9a29b88a20a620e195172f6fdf7e
Tufusa <louptung@gmail.com>
2023-08-31 10:09:59 +0900

    add: `users`テーブルに`nickname`カラムとインデックスを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_17_075505) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_29_150624) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -71,7 +71,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_17_075505) do
     t.datetime "activated_at"
     t.string "reset_digest"
     t.datetime "reset_sent_at"
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["nickname"], name: "index_users_on_nickname", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
