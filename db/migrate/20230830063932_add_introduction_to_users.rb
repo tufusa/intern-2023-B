a12689d816eb24d1976d913e4c1b45406ea99cee
hosogi <s215041@matsu.shimane-u.ac.jp>
2023-08-31 13:33:46 +0900

    自己紹介文の追加

--- db/migrate/20230830063932_add_introduction_to_users.rb
+++ db/migrate/20230830063932_add_introduction_to_users.rb
@@ -1,5 +1,5 @@
 class AddIntroductionToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :Introduce, :text
+    add_column :users, :introduce, :text
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -74,7 +74,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_30_063932) do
     t.datetime "activated_at"
     t.string "reset_digest"
     t.datetime "reset_sent_at"
-    t.text "Introduce"
+    t.text "introduce"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
