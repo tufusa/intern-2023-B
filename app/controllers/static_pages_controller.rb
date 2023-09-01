7d70e8bdd6106de71da41acb994b92bfa02a8c8f
hosogi <s215041@matsu.shimane-u.ac.jp>
2023-09-01 14:04:23 +0900

    経過時間が48時間以下かつフォローしている人だけで最新の10けんだけが表示されるように

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -17,5 +17,15 @@ class StaticPagesController < ApplicationController
   end
 
   def newpost
+    if logged_in?
+      passtime = 48.hours.ago
+      following_ids = "SELECT followed_id FROM relationships
+                        WHERE  follower_id = :user_id"
+      @newpost_items =  Micropost.where("user_id IN (#{following_ids}) AND created_at>= :passtime", user_id:current_user.id,passtime: passtime)
+                .includes(:user, image_attachment: :blob).limit(10)
+      @micropost  = current_user.microposts.build
+      
+     
+    end
   end
 end
