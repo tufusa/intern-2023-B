b8a67d6f778408e54979dc6e0ed6ecb1aaadf452
hosogi <s215041@matsu.shimane-u.ac.jp>
2023-09-01 16:10:16 +0900

    ログイン周りの問題を修正しました

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
-
+  before_action :logged_in_user, only: [:newpost]
   def home
     if logged_in?
       @micropost  = current_user.microposts.build
@@ -17,14 +17,10 @@ class StaticPagesController < ApplicationController
   end
 
   def newpost
-    if logged_in?
-      following_ids = "SELECT followed_id FROM relationships
-                        WHERE  follower_id = :user_id"
-      @newpost_items =  Micropost.where("user_id IN (#{following_ids}) AND created_at>= :passtime", user_id:current_user.id,passtime: Settings.newpost.limithour.hours.ago)
-                .includes(:user, image_attachment: :blob).limit(Settings.newpost.limitnum)
-      @micropost  = current_user.microposts.build
-      
-     
-    end
+    following_ids = "SELECT followed_id FROM relationships
+                      WHERE  follower_id = :user_id"
+    @newpost_items =  Micropost.where("user_id IN (#{following_ids}) AND created_at>= :passtime", user_id:current_user.id,passtime: Settings.newpost.limithour.hours.ago)
+              .includes(:user, image_attachment: :blob).limit(Settings.newpost.limitnum)
+    @micropost  = current_user.microposts.build
   end
 end
