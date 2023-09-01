dea8e1cb801cb4c37e7d4b5896fa9778788e52ae
hosogi <s215041@matsu.shimane-u.ac.jp>
2023-09-01 14:36:45 +0900

    指摘された点の修正

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -18,11 +18,10 @@ class StaticPagesController < ApplicationController
 
   def newpost
     if logged_in?
-      passtime = 48.hours.ago
       following_ids = "SELECT followed_id FROM relationships
                         WHERE  follower_id = :user_id"
-      @newpost_items =  Micropost.where("user_id IN (#{following_ids}) AND created_at>= :passtime", user_id:current_user.id,passtime: passtime)
-                .includes(:user, image_attachment: :blob).limit(10)
+      @newpost_items =  Micropost.where("user_id IN (#{following_ids}) AND created_at>= :passtime", user_id:current_user.id,passtime: Settings.newpost.limithour.hours.ago)
+                .includes(:user, image_attachment: :blob).limit(Settings.newpost.limitnum)
       @micropost  = current_user.microposts.build
       
      
