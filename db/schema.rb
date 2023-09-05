25cbc18253df1ec67081fa9ee7b0c17ffc42cffb
tufusa <tufusa74@gmail.com>
2023-09-05 13:25:51 +0900

    test: `Micropost.is_fixed`がtruthyになってしまいテストに失敗していたので、`Micropost.is_fixed`にデフォルト値`false`と非`NULL`制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_04_015240) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_04_135506) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -56,7 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_04_015240) do
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "is_fixed"
+    t.boolean "is_fixed", default: false, null: false
     t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
