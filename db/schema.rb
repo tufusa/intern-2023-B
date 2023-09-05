217f6087dd341faec7049091f2e9e1a74541f40a
hosogi <s215041@matsu.shimane-u.ac.jp>
2023-09-05 13:43:23 +0900

    データベースの変更(出身地カラムの追加)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_04_015240) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_05_043950) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -89,6 +89,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_04_015240) do
     t.datetime "reset_sent_at"
     t.text "introduce"
     t.string "nickname"
+    t.string "birthplace"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["nickname"], name: "index_users_on_nickname", unique: true
   end
