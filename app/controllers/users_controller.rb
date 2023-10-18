bd61f3736a13d5fa6f62092be5b80473b4fb5d5a
tufusa <tufusa74@gmail.com>
2023-10-18 16:13:59 +0900

    fix: activate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,8 +33,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      @user.send_activation_email
-      flash[:info] = "Please check your email to activate your account."
+      # @user.send_activation_email
+      # flash[:info] = "Please check your email to activate your account."
+      @user.activate
       redirect_to root_url
     else
       render 'new', status: :unprocessable_entity
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "user@realdomain.com"
+  default from: "noreply@example.com"
   layout "mailer"
 end
