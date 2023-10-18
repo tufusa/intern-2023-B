f53d730b6dbe38ee8e2d00656cbfc25894c54dae
tufusa <tufusa74@gmail.com>
2023-10-18 14:14:59 +0900

    chore: db

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,4 +4,5 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails db:migrate
+bundle exec rails db:migrate RAILS_ENV=development
+bundle exec rails db:seed
