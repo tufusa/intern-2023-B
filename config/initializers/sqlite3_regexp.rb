8f54006dbf832795edaf723897e00b1f3b37b208
tufusa <tufusa74@gmail.com>
2023-10-19 22:18:13 +0900

    feat: regexp ext

--- config/initializers/sqlite3_regexp.rb
+++ config/initializers/sqlite3_regexp.rb
@@ -12,3 +12,20 @@ class ActiveRecord::ConnectionAdapters::SQLite3Adapter
     end
   end
 end
+
+if ENV['DB_NAME'] == 'production'
+  require 'activerecord/lib/active_record/connection_adapters/postgresql_adapter'
+
+  class ActiveRecord::ConnectionAdapters::PostgreSQLAdapter
+    alias old_initialize initialize
+    private :old_initialize
+  
+    def initialize(connection, *args)
+      old_initialize(connection, *args)
+  
+      connection.create_function('REGEXP', 2) do |func, pattern, expression|
+        func.result = /#{pattern}/.match?(expression.to_s) ? 1 : 0
+      end
+    end
+  end
+end
