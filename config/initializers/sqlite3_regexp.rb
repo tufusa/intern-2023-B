eaa1f570dc13aacea26abd0ca2e6ebc34105b857
tufusa <tufusa74@gmail.com>
2023-10-19 22:40:51 +0900

    fix: delete pg ext

--- config/initializers/sqlite3_regexp.rb
+++ config/initializers/sqlite3_regexp.rb
@@ -12,20 +12,3 @@ class ActiveRecord::ConnectionAdapters::SQLite3Adapter
     end
   end
 end
-
-if ENV['DB_MODE'] == 'production'
-  require 'activerecord/connection_adapters/postgresql_adapter'
-
-  class ActiveRecord::ConnectionAdapters::PostgreSQLAdapter
-    alias old_initialize initialize
-    private :old_initialize
-  
-    def initialize(connection, *args)
-      old_initialize(connection, *args)
-  
-      connection.create_function('REGEXP', 2) do |func, pattern, expression|
-        func.result = /#{pattern}/.match?(expression.to_s) ? 1 : 0
-      end
-    end
-  end
-end
